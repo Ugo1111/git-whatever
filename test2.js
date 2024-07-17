177eb8068ca020096491cdcebb8e81ec35addd56
Ugo1111 <dirleeugo@gmail.com>
2024-07-17 19:03:46 +0100

    header has been added

---
+++ test2.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'grey',
+   // alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  title: {
+    paddingTop:10,
+    fontSize: 30,
+    color: 'black',
+   // alignItems: 'center',
+    //justifyContent: 'center',
+  },
+  taskContainer: {
+
+  },
+button: {
+  color: 'blue'
+},
+task: {
+
+},
+circle: {
+
+},
+});
